4ca9f079c47078f4c6e0364e68fb5bbae9347cf1
aditya413512 <adityasalunke413512@gmail.com>
2022-09-12 21:28:32 +0530

    concate

--- string/concate.c
+++ string/concate.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
-
+#include<string.h>
 int main()
 {
-    char s1[10],s2[10];
+    char s1[20],s2[10];
     int i,j;
+    printf("enter a 1st string: ");
     gets(s1);
+
+    printf("enter a 2nd string: ");
     gets(s2);
 
-    for(i=0;s1[i]!='\0',i++);
-    for()
+   for(i=0;s1[i]!='\0';i++);
+   for(j=0;s2[j]!='\0';i++,j++)
+   {
+    s1[i]=s2[j];
+   }
+   s1[i]='\0';
+   printf("%s\n",s1);
 
 }
